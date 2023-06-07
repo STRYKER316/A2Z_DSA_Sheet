32e33b1eba269c08df06f0fb654217f494c663d2
Stryker316 <ansil.mishra316@gmail.com>
2023-06-07 21:18:39 +0530

    Majority Element(n/2) - medium

---
+++ Arrays/2_Medium/Majority_element_1.py
@@ -0,0 +1,24 @@
+# ------- Optimal Approach (Moore's Voting Algorithm) -------
+# O(N) TC | O(1) SC
+
+def majorityElement(array):
+    majorityNumber = None
+    majorityCount = 0
+    for num in array:
+        if num == majorityNumber:
+            majorityCount += 1
+        elif majorityCount == 0:
+            majorityNumber = num
+            majorityCount = 1
+        else:
+            majorityCount -= 1
+    
+    # verify the majority element
+    actualCount = 0
+    for num in array:
+        if num == majorityNumber:
+            actualCount += 1
+    
+    if actualCount > len(array) // 2:
+        return majorityNumber
+    return -1
