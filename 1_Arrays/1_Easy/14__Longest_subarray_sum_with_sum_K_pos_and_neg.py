1555b81bc83cc2b23bdbeaef94e8e63dc59645e3
Stryker316 <ansil.mishra316@gmail.com>
2023-06-09 22:15:25 +0530

    Longest subarray sum with sum K (Pos + Neg)

---
+++ 1_Arrays/1_Easy/14__Longest_subarray_sum_with_sum_K_pos_and_neg.py
@@ -0,0 +1,30 @@
+# --------- HashMap approach ---------
+# O(N) TC | O(N) SC
+ 
+def lenOfLongSubarr (arr, k) : 
+    sumTillIndexMap = {}
+    maxLengthSubarray = 0
+
+    runningSum = 0
+    for i in range(len(arr)):
+        # record the sum till current index
+        runningSum += arr[i]
+        if runningSum not in sumTillIndexMap:
+            sumTillIndexMap[runningSum] = i
+
+        # case 1
+        if runningSum == k:
+            currentLength = i + 1
+            maxLengthSubarray = max(maxLengthSubarray, currentLength)
+        # case 2
+        requiredSum = runningSum - k
+        if requiredSum in sumTillIndexMap:
+            currentLength = i - sumTillIndexMap[requiredSum]
+            maxLengthSubarray = max(maxLengthSubarray, currentLength)
+    
+    return maxLengthSubarray
+
+
+
+# Link: https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
+# Link: https://www.codingninjas.com/codestudio/problems/longest-subarray-with-sum-k_5713505
