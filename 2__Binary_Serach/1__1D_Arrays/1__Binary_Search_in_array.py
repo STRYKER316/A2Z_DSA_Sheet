5c3569add4883056c2d40c0915c11fe0f0e0f4d0
Stryker316 <ansil.mishra316@gmail.com>
2023-06-10 20:39:21 +0530

    Binary Search in Sorted Array

---
+++ 2__Binary_Serach/1__1D_Arrays/1__Binary_Search_in_array.py
@@ -0,0 +1,46 @@
+# ----------- Itearative Solution -----------
+# O(log(n)) TC | O(1) SC
+
+def binarysearch(arr, n, k):
+    leftIdx, rightIdx = 0, n - 1
+    midIdx = 0
+
+    while leftIdx <= rightIdx:
+        midIdx = (leftIdx + rightIdx) // 2
+        # check for the element
+        if arr[midIdx] == k:
+            return midIdx
+        elif arr[midIdx] < k:
+            leftIdx = midIdx + 1
+        elif arr[midIdx] > k:
+            rightIdx = midIdx - 1
+
+    return -1
+
+
+# ---------- Recursive Solution -------
+# O(log(n)) TC | O(log(n)) SC
+
+def binarySearch(arr, n, k):
+    leftIdx, rightIdx = 0, n - 1
+    return binarySearchHelper(arr, k, leftIdx, rightIdx)
+
+
+# Helper Method
+def binarySearchHelper(array, target, leftIdx, rightIdx):
+    # Base case
+    if leftIdx > rightIdx:
+        return -1    
+    
+    midIdx = (leftIdx + rightIdx) // 2
+    if array[midIdx] == target:
+        return midIdx
+    elif array[midIdx] < target:
+        return binarySearchHelper(array, target, midIdx + 1, rightIdx)
+    elif array[midIdx] > target:
+        return binarySearchHelper(array, target, leftIdx, midIdx - 1)
+
+
+
+# Link: https://bit.ly/3QtKBpO
+# Link: https://leetcode.com/problems/binary-search/
