66d4b670477c3ae6677df290d674bdbdbc295768
Stryker316 <ansil.mishra316@gmail.com>
2023-06-11 10:21:39 +0530

    Floor/Ceil in sorted array - 1D Binary Search_5

---
+++ 2__Binary_Serach/1__1D_Arrays/5__Floor_Ceil_in_sorted_array.py
@@ -0,0 +1,27 @@
+# O(log(arrayLen)) TC | O(1) SC
+
+def ceilingInSortedArray(arr, arrayLen, target):
+    floorVal = -1
+    ceilVal = -1
+
+    leftIdx, rightIdx = 0, arrayLen - 1
+    midIdx = 0
+    while leftIdx <= rightIdx:
+        midIdx = (leftIdx + rightIdx) // 2
+        # condition check
+        if arr[midIdx] == target:
+            floorVal = arr[midIdx]
+            ceilVal = arr[midIdx]
+            break
+        elif arr[midIdx] < target:
+            floorVal = arr[midIdx]
+            leftIdx = midIdx + 1
+        elif arr[midIdx] > target:
+            ceilVal = arr[midIdx]
+            rightIdx = midIdx - 1
+
+    return [floorVal, ceilVal]
+
+
+
+# Link: https://www.codingninjas.com/codestudio/problems/ceiling-in-a-sorted-array_1825401
