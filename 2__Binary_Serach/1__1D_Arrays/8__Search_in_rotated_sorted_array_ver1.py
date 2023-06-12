f98fe327904f8230c65fff0e01433d420abfd82f
Stryker316 <ansil.mishra316@gmail.com>
2023-06-12 22:52:46 +0530

    Search in rotated sorted array (ver 1) - 1D Binary_8

---
+++ 2__Binary_Serach/1__1D_Arrays/8__Search_in_rotated_sorted_array_ver1.py
@@ -0,0 +1,31 @@
+# O(log(n)) TC | O(1) SC
+
+def search(arr, n, k):
+	# k -> target, n -> arrayLen
+	leftIdx, rightIdx = 0, n - 1
+	midIdx = 0
+
+	ansIdx = -1
+	while leftIdx <= rightIdx:
+		midIdx = (leftIdx + rightIdx) // 2
+
+		if arr[midIdx] == k:
+			ansIdx = midIdx
+			break
+		# check if left-half is sorted or not
+		elif arr[leftIdx] <= arr[midIdx]:
+			if arr[leftIdx] <= k and k <= arr[midIdx]:
+				rightIdx = midIdx - 1
+			else:   leftIdx = midIdx + 1
+		# check if right-half is sorted or not
+		elif arr[rightIdx] >= arr[midIdx]:
+			if arr[midIdx] <= k and k <= arr[rightIdx]:
+				leftIdx = midIdx + 1
+			else:   rightIdx = midIdx - 1
+	
+	return ansIdx
+
+
+# GFG: https://bit.ly/3QH471y
+# CN: https://www.codingninjas.com/codestudio/problems/search-in-rotated-sorted-array_1082554
+# Leetcode: https://leetcode.com/problems/search-in-rotated-sorted-array/
