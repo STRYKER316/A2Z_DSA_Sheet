10d6434c808e85401eb436e519dce68ccc516a67
Stryker316 <ansil.mishra316@gmail.com>
2023-06-15 13:24:16 +0530

    Find Minimum in rotated sorted array - 1D BS_10

---
+++ 2__Binary_Serach/1__1D_Arrays/10__Minimum_in_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+# O(log(arrayLen)) TC | O(1) SC
+
+def findMin(nums):
+    # no rotation check
+    if nums[0] < nums[-1]:    return nums[0]
+    
+    minValue = float('inf')
+    leftIdx, rightIdx = 0, len(nums) - 1
+    midIdx = 0
+    
+    while leftIdx <= rightIdx:
+        midIdx = (leftIdx + rightIdx) // 2
+        # check if left half is sorted
+        if nums[leftIdx] <= nums[midIdx]:
+            minValue = min(minValue, nums[leftIdx])
+            leftIdx = midIdx + 1
+        # else right half is sorted
+        else:
+            minValue = min(minValue, nums[midIdx])
+            rightIdx = midIdx - 1
+    
+    return minValue
+
+
+
+# GFG: https://bit.ly/3Az1OJ3
+# Leetcode: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+# CN: https://www.codingninjas.com/codestudio/problems/rotated-array_1093219
