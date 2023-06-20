2683470956e45fddb210c3e15d2e170dddfea84e
Stryker316 <ansil.mishra316@gmail.com>
2023-06-20 21:48:15 +0530

    Largest subarray with 0 sum - Arrays_Hard_05

---
+++ 1_Arrays/3_Hard/05__Largest_subarray_with_0_sum.py
@@ -0,0 +1,32 @@
+# O(N) TC | O(N) SC     [N -> Length of array]
+
+def maxLen(arr, n):
+    prefixSumIndexMap = {}
+    maxLengthSubarray = float('-inf')
+    
+    runningPrefixSum = 0
+    for i in range(n):
+        runningPrefixSum += arr[i]
+        
+        # case 1
+        if runningPrefixSum == 0:
+            currentSubarrayLength = i + 1
+            maxLengthSubarray = max(currentSubarrayLength, maxLengthSubarray)
+        
+        # case 2
+        if runningPrefixSum in prefixSumIndexMap:
+            currentSubarrayLength = i - prefixSumIndexMap[runningPrefixSum]
+            maxLengthSubarray = max(currentSubarrayLength, maxLengthSubarray)
+        
+        # record the prefixSum
+        if runningPrefixSum not in prefixSumIndexMap:
+            prefixSumIndexMap[runningPrefixSum] = i
+    
+    # check answer validity
+    if maxLengthSubarray == float('-inf'):
+        return 0
+    return maxLengthSubarray
+
+
+
+# GFG: https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1
