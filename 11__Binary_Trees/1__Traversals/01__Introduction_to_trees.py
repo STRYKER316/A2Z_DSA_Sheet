781a881a13cf7b41fc9e00cd30bdf0fbaba88dca
Stryker316 <ansil.mishra316@gmail.com>
2023-07-01 09:58:04 +0530

    Intro to Trees - BinaryTrees_Traversals_Q1

---
+++ 11__Binary_Trees/1__Traversals/01__Introduction_to_trees.py
@@ -0,0 +1,8 @@
+# O(1) TC | O(1) SC
+
+def countNodes(level):
+    maxNodeCount = 2 ** (level - 1)
+    return maxNodeCount
+
+
+# GFG: https://practice.geeksforgeeks.org/problems/introduction-to-trees/1
