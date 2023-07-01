714f514c317c9307dea77827f1aa6b711d80c239
Stryker316 <ansil.mishra316@gmail.com>
2023-07-01 11:23:58 +0530

    Preorder traversal - BinaryTrees_Traversal_Q03

---
+++ 11__Binary_Trees/1__Traversals/03__Preorder_traversal.py
@@ -0,0 +1,30 @@
+# Recursive Solution    [N -> Number of tree nodes]
+# O(N) TC   [Visit all the nodes of the tree once]
+# O(N) SC   [Worst case scenario -> Skewed Tree]
+
+def preorderTraversal( root):
+    traversalResult = []
+    # edge case
+    if root == None:
+        return traversalResult
+
+    # function to populate the preorder traversal result
+    preorderTraversal(root, traversalResult)
+    return traversalResult
+
+
+# Helper Method
+def preorderTraversal(root, traversalResult):
+    # base case
+    if root == None:
+        return
+    
+    # logic
+    traversalResult.append(root.val)
+    preorderTraversal(root.left, traversalResult)
+    preorderTraversal(root.right, traversalResult)
+
+
+
+# Leetcode: https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+# GFG: https://practice.geeksforgeeks.org/problems/preorder-traversal/1
