29928f4e3fbe2d5f7bfc82af19553b6582b05b8c
Stryker316 <ansil.mishra316@gmail.com>
2023-07-03 11:14:46 +0530

    Iterative Postorder Traversal using 1 Stack - BinaryTrees_Traversals_Q11

---
+++ 11__Binary_Trees/1__Traversals/11__Iterative_postorder_traversal_version2.py
@@ -0,0 +1,30 @@
+# O(N) TC | O(N) SC     [N -> Number of Treenodes]
+
+from collections import deque
+
+def postorderTraversal(root):
+    # edge case
+    if root == None:    return []
+
+    nodeStack = deque()
+    nodeStack.append(root)
+    traversalResult = []
+
+    while len(nodeStack) > 0:
+        # process the top node
+        topNode = nodeStack.pop()
+        traversalResult.append(topNode.val)
+
+        # push left and right children into stack, if there are any
+        if topNode.left != None:
+            nodeStack.append(topNode.left)
+        if topNode.right != None:
+            nodeStack.append(topNode.right)
+    
+    # reverse the result to get the postorder traversal
+    traversalResult.reverse()
+    return traversalResult
+
+
+
+# Leetcode: https://leetcode.com/problems/binary-tree-postorder-traversal/
