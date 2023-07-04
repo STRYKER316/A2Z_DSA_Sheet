f226ca0c2e69eb563cc4b7541d2460734c0fb786
Stryker316 <ansil.mishra316@gmail.com>
2023-07-04 11:09:21 +0530

    Max Depth/Height of Binary tree (Recursive Solution) - BinaryTrees_Medium_Q01

---
+++ 11__Binary_Trees/2__Medium_Problems/01__Max_depth_or_height_of_binary_tree.py
@@ -0,0 +1,28 @@
+# ------------------- Recursive Solution -------------------
+# O(N) TC | O(H) SC     [N -> Number of tree nodes | H -> Height of Tree]
+
+def height(root):
+    if root == None:
+        return 0
+    return getMaxDepth(root)
+
+
+# Helper Recursive Method
+def getMaxDepth(root):
+    # base case
+    if root == None:
+        return 0
+    # logic
+    leftSubtreeMaxDepth = getMaxDepth(root.left)
+    rightSubtreeMaxDepth = getMaxDepth(root.right)
+    return 1 + max(leftSubtreeMaxDepth, rightSubtreeMaxDepth)
+
+
+# ------------------- Iterative Solution -------------------
+def height(root):
+    pass
+
+
+
+# GFG: https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+# Leetcode: https://leetcode.com/problems/maximum-depth-of-binary-tree/
