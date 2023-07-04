46d5948eaf9ad05c8562458e2c78bb1f9fe6bbbd
Stryker316 <ansil.mishra316@gmail.com>
2023-07-04 15:13:15 +0530

    Code Modification - BinaryTrees_Medium_Q05

--- 11__Binary_Trees/2__Medium_Problems/05__Check_if_two_trees_are_identical.py
+++ 11__Binary_Trees/2__Medium_Problems/05__Check_if_two_trees_are_identical.py
@@ -2,18 +2,13 @@
 # O(min(N1, N2)) TC | O(min(H1, H2)) SC     [Nx -> Number of Nodes of tree x | Hx -> Height of tree x]
 
 def isIdentical(root1, root2):
-    return areIdentical(root1, root2)
-
-# Helper Recursive Method
-def areIdentical(root1, root2):
     # base cases
     if root1 == None and root2 == None:
         return True
     if root1 == None or root2 == None:
         return False
     # logic
-    return root1.data == root2.data and areIdentical(root1.left, root2.left) and areIdentical(root1.right, root2.right)
-
+    return root1.data == root2.data and isIdentical(root1.left, root2.left) and isIdentical(root1.right, root2.right)
 
 
 # --------------- Iterative Solution ---------------
