b6e6823200e82c318d3ab18f053b3afe601c1361
Stryker316 <ansil.mishra316@gmail.com>
2023-07-07 13:55:20 +0530

    Symmtric Binary Tree - BinaryTrees_Medium_Q13

---
+++ 11__Binary_Trees/2__Medium_Problems/13__Symmetric_binary_tree.py
@@ -0,0 +1,26 @@
+# O(N) TC | O(H) SC     [N -> Numbr of tree nodes | H -> Height of tree]
+
+def isSymmetric(root):
+    # edge case
+    if root == None:
+        return True
+    # if the subtrees are mirror images of each othr, then the main tree is symmetric
+    return areMirrorImageTrees(root.left, root.right)
+
+
+# Helper Method
+def areMirrorImageTrees(root1, root2):
+    # base case
+    if root1 == None and root2 == None:
+        return True
+    if root1 == None or root2 == None:
+        return False
+
+    # logic
+    return root1.data == root2.data and areMirrorImageTrees(root1.left, root2.right) and areMirrorImageTrees(root1.right, root2.left)
+
+
+
+# Leetcode: https://leetcode.com/problems/symmetric-tree/
+# GFG: https://practice.geeksforgeeks.org/problems/symmetric-tree/1
+# CN: https://www.codingninjas.com/studio/problems/630426
