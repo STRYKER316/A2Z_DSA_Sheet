e4c8bc160b9ce835dae7a184f8a9f370990b435f
Stryker316 <ansil.mishra316@gmail.com>
2023-07-11 12:20:59 +0530

    Introduction to Binary Search Tree - BST_Concepts_Q01

---
+++ 12__Binary_Search_Trees/1__Concepts/01__Introduction_to_BST.py
@@ -0,0 +1,13 @@
+def isValidBST(inOrder):
+    # check if the inOrder array is sorted in increasing fashion or not
+    isValid = True
+    for i in range(1, len(inOrder)):
+        if inOrder[i] <= inOrder[i - 1]:
+            isValid = False
+            break
+    
+    return isValid
+
+
+
+# CN: https://www.codingninjas.com/studio/problems/binary-search-trees_8160443
