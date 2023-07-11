5a8f6fc22136f4184fce8df86bf614ab1191f488
Stryker316 <ansil.mishra316@gmail.com>
2023-07-11 12:54:35 +0530

    Ceil value in a BST - BST_Problems_Q01

---
+++ 12__Binary_Search_Trees/2__Problems/01__Ceil_value_in_BST.py
@@ -0,0 +1,20 @@
+# O(logN) TC | O(1) SC      [N - > Number of Tree nodes]
+
+def findCeil(root, x):
+    ceilValue = -1
+
+    # keep searching tillwe run out of possible nodes to search
+    while root != None:
+        if root.data == x:
+            return root.data
+        # record the root-value, whenever the root-value > x and then search for a smaller value
+        if root.data > x:
+            ceilValue = root.data
+            root = root.left
+        else:   root = root.right
+
+    return ceilValue
+
+
+
+# CN: CN: https://www.codingninjas.com/studio/problems/ceil-from-bst_920464
